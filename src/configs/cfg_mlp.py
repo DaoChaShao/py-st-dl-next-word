27a8bd11eae893f5aeb9f4f577111700aff9a0ba
DaoChaShao <27882272@qq.com>
2025-12-03 00:12:37 +0800

    feat: add configuration module for MLP parameters and preprocessing

---
+++ src/configs/cfg_mlp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/11/29 15:18
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   cfg_mlp.py
+# @Desc     :   
+
+from dataclasses import dataclass, field
+
+from src.configs.cfg_base import FilePaths
+from src.configs.cfg_base4dl import DataPreprocessor, Hyperparameters
+
+
+@dataclass
+class MLPParams:
+    UNITS: int = 128
+
+
+@dataclass
+class Configuration4MLP:
+    FILEPATHS: FilePaths = field(default_factory=FilePaths)
+    HYPERPARAMETERS: Hyperparameters = field(default_factory=Hyperparameters)
+    PARAMETERS: MLPParams = field(default_factory=MLPParams)
+    PREPROCESSOR: DataPreprocessor = field(default_factory=DataPreprocessor)
+
+
+CONFIG4MLP = Configuration4MLP()

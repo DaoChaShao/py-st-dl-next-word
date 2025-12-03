480b73d4b696daf0a82ee5bc084324558c580c77
DaoChaShao <27882272@qq.com>
2025-12-04 03:08:13 +0800

    feat: add Database configuration and update RNN parameters in cfg_rnn.py

--- src/configs/cfg_rnn.py
+++ src/configs/cfg_rnn.py
@@ -8,21 +8,22 @@
 
 from dataclasses import dataclass, field
 
-from src.configs.cfg_base import FilePaths
+from src.configs.cfg_base import FilePaths, Database
 from src.configs.cfg_base4dl import DataPreprocessor, Hyperparameters
 
 
 @dataclass
 class RNNParams:
     CLASSES: int = 2  # Binary classification is 2
-    EMBEDDING_DIM: int = 256
-    HIDDEN_SIZE: int = 64
+    EMBEDDING_DIM: int = 128
+    HIDDEN_SIZE: int = 256
     LAYERS: int = 2
     TEMPERATURE: float = 1.0
 
 
 @dataclass
 class Configuration4RNN:
+    DATABASE: Database = field(default_factory=Database)
     FILEPATHS: FilePaths = field(default_factory=FilePaths)
     HYPERPARAMETERS: Hyperparameters = field(default_factory=Hyperparameters)
     PARAMETERS: RNNParams = field(default_factory=RNNParams)

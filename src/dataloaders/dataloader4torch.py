3fe4de8ffedfd111ed1f2d0d1771b3e03294f343
DaoChaShao <27882272@qq.com>
2025-12-05 15:25:12 +0800

    feat: enhance TorchDataLoader to include workers parameter and update shuffle handling

--- src/dataloaders/dataloader4torch.py
+++ src/dataloaders/dataloader4torch.py
@@ -4,7 +4,7 @@
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
 # @File     :   dataloader4torch.py
-# @Desc     :   
+# @Desc     :
 
 from torch import Tensor
 from torch.utils.data import DataLoader, Dataset
@@ -13,20 +13,30 @@ from torch.utils.data import DataLoader, Dataset
 class TorchDataLoader:
     """ A custom PyTorch DataLoader class for handling TorchDataset """
 
-    def __init__(self, dataset: Dataset, batch_size: int = 32, is_shuffle: bool = True):
+    def __init__(self,
+                 dataset: Dataset,
+                 batch_size: int = 32, shuffle_state: bool = True,
+                 workers: int = 0,
+                 ):
         """ Initialise the TorchDataLoader class
         :param dataset: the TorchDataset or Dataset to load data from
         :param batch_size: the number of samples per batch
-        :param is_shuffle: whether to shuffle the data at every epoch
+        :param shuffle_state: whether to shuffle the data at every epoch
+        :param workers: the number of workers to use for data loading
         """
         self._dataset: Dataset = dataset
         self._batches: int = batch_size
-        self._is_shuffle: bool = is_shuffle
+        self._shuffle: bool = shuffle_state
 
+        pin_memory: bool = False if workers == 0 else True
+        print(f"num_workers={workers} | pin_memory={pin_memory}")
+        print()
         self._loader: DataLoader = DataLoader(
             dataset=self._dataset,
             batch_size=self._batches,
-            shuffle=self._is_shuffle,
+            shuffle=self._shuffle,
+            num_workers=workers,
+            pin_memory=pin_memory,
         )
 
     @property
@@ -48,7 +58,7 @@ class TorchDataLoader:
     def __repr__(self):
         return (f"TorchDataLoader(dataset={self._dataset}, "
                 f"batch_size={self._batches}, "
-                f"shuffle={self._is_shuffle})")
+                f"shuffle={self._shuffle})")
 
 
 if __name__ == "__main__":

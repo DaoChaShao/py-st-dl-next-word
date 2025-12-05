9705bbd87829110b98f585a61504cd9d05287837
DaoChaShao <27882272@qq.com>
2025-12-05 15:24:39 +0800

    feat: update calculator_for_regression to accept lists and convert to ndarray

--- src/trainers/calc4regression.py
+++ src/trainers/calc4regression.py
@@ -6,26 +6,31 @@
 # @File     :   calc4regression.py
 # @Desc     :   
 
-from numpy import ndarray, sqrt
+from numpy import ndarray, sqrt, array
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
-def calculator_for_regression(outputs: ndarray, targets: ndarray) -> dict[str, float]:
+def calculator_for_regression(outputs: list[int], targets: list[int]) -> dict[str, float]:
     """ Calculates the mean squared error and mean absolute error
     :param outputs: the predicted outputs
     :param targets: the ground truth outputs
     :return: a dict containing the mean squared error and mean absolute error
     """
-    rMse = sqrt(((outputs - targets) ** 2).mean())
-    mse = mean_squared_error(outputs, targets)
-    mae = mean_absolute_error(outputs, targets)
-    r2 = r2_score(targets, outputs)
+    outputs: ndarray = array(outputs)
+    targets: ndarray = array(targets)
+
+    dps: int = 4
+
+    rMse: float = sqrt(((outputs - targets) ** 2).mean())
+    mse: float = mean_squared_error(outputs, targets)
+    mae: float = mean_absolute_error(outputs, targets)
+    r2: float = r2_score(targets, outputs)
 
     return {
-        "rMse": rMse,
-        "mse": mse,
-        "mae": mae,
-        "r2": r2
+        "rMse": round(rMse, dps),
+        "mse": round(mse, dps),
+        "mae": round(mae, dps),
+        "r2": round(r2, dps),
     }
 
 
